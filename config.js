74a5e1c7e6c51726ba7cbb7362f1184b9278ae13
zudazhuang <522025360052@smail.nju.edu.cn>
2026-06-23 23:22:49 +0800

    Use Supabase backend for expert review

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ window.EXPERT_REVIEW_PRIMARY_BACKEND = {
   type: "supabase",
   submitUrl: "",
   listUrl: "",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://qdhzczmmszrgvswaoqgl.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkaHpjem1tc3pyZ3Zzd2FvcWdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMjI1MjgsImV4cCI6MjA5Nzc5ODUyOH0.gl6w4HmmVrGDCyNZ3s1LP4-cLDFBcyo3YYTaQ1wimfc",
   tableName: "expert_review_submissions",
 };
 window.EXPERT_REVIEW_BACKEND = {
   type: "crudcrud",
-  baseUrl: "https://crudcrud.com/api/af35b181c6124950adcad457cabf9d2a",
+  baseUrl: "",
 };
