0b1ca07a6cf3232a0097c7e610c83661e3e71aa2
zudazhuang <522025360052@smail.nju.edu.cn>
2026-06-23 19:14:05 +0800

    Refresh expert review backend endpoint

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.EXPERT_REVIEW_SUBMIT_ENDPOINT = "";
 window.EXPERT_REVIEW_SUBMIT_MODE = "no-cors";
 window.EXPERT_REVIEW_BACKEND = {
   type: "crudcrud",
-  baseUrl: "https://crudcrud.com/api/a7e7c5d7836d4dbbb774e166126658a6",
+  baseUrl: "https://crudcrud.com/api/380f68c9ee2f46868773243f05436622",
 };
